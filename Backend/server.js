4a5f7beb08ecfe8180c42692c2cdba7688d073ef
Prathmesh kulkarni <prathmeshkulkarni312@gmail.com>
2025-02-19 18:41:09 +0530

    CHANGED CONGIF AND ROUTES7

--- Backend/server.js
+++ Backend/server.js
@@ -36,10 +36,9 @@ db.once("open", () => console.log("Connected to MongoDB Atlas"));
 // Flashcard Schema
 app.use("/api/flashcard",f1);
 
+
 // Start Server
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
-});
+
 app.get("/",(req,res)=>{
   res.send("flashcard api is running");
 });
